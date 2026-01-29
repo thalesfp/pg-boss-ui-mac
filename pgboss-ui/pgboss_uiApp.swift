8b9ea0ebdc653c73c1be13bab35a519115ee3f43
Thales Pereira <thalesfp@gmail.com>
2026-01-29 11:27:17 -0800

    Initial Commit

---
+++ pgboss-ui/pgboss_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pgboss_uiApp.swift
+//  pgboss-ui
+//
+//  Created by thales on 2026-01-29.
+//
+
+import SwiftUI
+
+@main
+struct pgboss_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
